866e312e5aa7f3f4b4af76bce1494343f9acc687
himideb <49477075+himideb@users.noreply.github.com>
2019-04-28 16:55:47 +0530

    Temparature file uploaded
    File containing code on temperature sensor

---
+++ Health_monitoring_ver1.ino
@@ -0,0 +1,15 @@
+#include<lm35.h>
+
+lm35 temps(1);
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+
+ temps.MeasureTemp();
+ Serial.print(temps.TempInCelcius + 20);
+ Serial.println("C");
+delay(500);
+}
