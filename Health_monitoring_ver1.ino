23b508fffce6082cb038303e86d07ae54ad70815
himideb <49477075+himideb@users.noreply.github.com>
2019-05-01 12:00:30 +0530

    Added_updated_file
    Main_file_is_updated

--- Health_monitoring_ver1.ino
+++ Health_monitoring_ver1.ino
@@ -1,15 +1,27 @@
 #include<lm35.h>
+int PulsePin = 0;
+int LED13 = 13;
+int Signal;
+int Threshold = 1500;
 
 lm35 temps(1);
 
 void setup() {
  Serial.begin(9600);
+ pinMode(LED13, OUTPUT);
 }
 
 void loop() {
-
+ Signal = analogRead(PulsePin);
+  Serial.println("BPM is: ", Signal);
+  if(Signal > Threshold){
+    digitalWrite(LED13, HIGH);    
+    }else{
+      digitalWrite(LED13, HIGH);
+      }
  temps.MeasureTemp();
  Serial.print(temps.TempInCelcius + 20);
  Serial.println("C");
 delay(500);
+
 }
